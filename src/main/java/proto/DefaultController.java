39107195c799f9da9beed54e5badf7300b768cea
Sébastien Demanou <demsking@gmail.com>
2017-12-04 08:05:02 +0100

    Add Vue & Webpack

--- src/main/java/proto/DefaultController.java
+++ src/main/java/proto/DefaultController.java
@@ -13,25 +13,15 @@ public class DefaultController {
 
     @Autowired
     private UserRepository repository;
-    
-    /**
-     * 初期画面を表示する
-     * @return 初期画面へ遷移
-     */
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
+
+    @RequestMapping("/")
     public String index() {
         return "index";
     }
 
-    /**
-     * データを設定する
-     * @return 初期画面へ遷移
-     */
     @RequestMapping(value = "/setData", method = RequestMethod.GET)
     public String setData() {
-        repository.save(new User("一郎", "舞黒", "説明１"));
-        repository.save(new User("二郎", "舞黒", "説明２"));
-        repository.save(new User("三郎", "舞黒", "説明３"));
+        repository.save(new User("1", "Jonh", "Snow"));
         return "index";
     }
 }
