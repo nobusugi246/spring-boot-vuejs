f32a703141f67102a29db21b87616ac5485a51ee
Sébastien Demanou <demsking@gmail.com>
2017-12-04 06:42:19 +0100

    Remove unnecessary files

--- src/main/resources/static/application.js
+++
@@ -1,9 +0,0 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    items: [
-      { firstName: '一郎', lastName: '舞黒', description: '説明１' },
-      { firstName: '二郎', lastName: '舞黒', description: '説明２' },
-      { firstName: '三郎', lastName: '舞黒', description: '説明３' }
-    ]  }
-})
