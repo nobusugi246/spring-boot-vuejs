4b1be1b24097403c15357f8a759b6aabeeaba88e
Sébastien Demanou <demsking@gmail.com>
2017-12-05 06:48:16 +0100

    Use vue-router history mode and add some options

--- src/vue/router/index.js
+++ src/vue/router/index.js
@@ -5,11 +5,24 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: __dirname,
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
     }
-  ]
+  ],
+  transitionOnLoad: true,
+  scrollBehavior (to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        selector: to.hash,
+        offset: { x: 0, y: 10 }
+      }
+    }
+
+    return { x: 0, y: 0 }
+  }
 })
