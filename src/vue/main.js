930a09881896cb48dd63e73120fcb670dd49911f
Sébastien Demanou <demsking@gmail.com>
2017-12-07 04:46:14 +0100

    Enable config.productionTip and config.performance on development mode

--- src/vue/main.js
+++ src/vue/main.js
@@ -4,7 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = process.env.NODE_ENV === 'development'
+Vue.config.performance = process.env.NODE_ENV === 'development'
 
 /* eslint-disable no-new */
 new Vue({
